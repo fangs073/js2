df4874d796c0326b2b1b7befeb0b346fc81d47e0
Student <student@RU-003.local>
2016-01-18 10:38:18 -0700

    Onwards

---
+++ js2.js
@@ -0,0 +1,3 @@
+var word = prompt ("Please enter a word", "Tnetennba");
+
+alert("You entered: " + word + "\n" + "There are " + word.length + " characters in the word." + "\n" + "The third character is '" + word[4] + "'\n" + word.toUpperCase() +"\n" + word.toLowerCase() + "\n" + word.slice(2,2) +"\n" + "Good evening! That's a nice " + word);
